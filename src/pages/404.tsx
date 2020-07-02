d358e229a938cdf4b2f0e5bfc59b0a8cf76ec3f6
forrest <tao123456>
2020-07-02 09:26:05 +0800

    初始化

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.css';
+
+export default function () {
+  return (
+    <div>你要访问的页面不存在</div>
+  );
+}
---
+++ src/pages/hello.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './index.css';
+
+export default function () {
+  return (
+    <div className={styles.normal}>
+      <ul className={styles.list}>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
